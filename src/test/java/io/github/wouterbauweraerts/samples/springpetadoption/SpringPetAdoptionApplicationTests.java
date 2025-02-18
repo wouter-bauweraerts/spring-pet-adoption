f0dc94b6e8969ac87bb9812c49ec2ef6de9d57a2
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2025-02-18 09:25:09 +0100

    Add spring modulith

--- src/test/java/io/github/wouterbauweraerts/samples/springpetadoption/SpringPetAdoptionApplicationTests.java
+++ src/test/java/io/github/wouterbauweraerts/samples/springpetadoption/SpringPetAdoptionApplicationTests.java
@@ -3,13 +3,22 @@ package io.github.wouterbauweraerts.samples.springpetadoption;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class SpringPetAdoptionApplicationTests {
 
     @Test
-    void contextLoads() {
+    void moduleCheck() {
+        ApplicationModules modules = ApplicationModules.of(SpringPetAdoptionApplication.class);
+        modules.verify();
+
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
     }
 
 }
